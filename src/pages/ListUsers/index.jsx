d154841b248fe146aa0de0c6713886ccaa2a54d4
Gabriel Castro <castrovisionario@icloud.com>
2026-05-10 23:54:08 -0300

    feat: add hooks useEffect and useState

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react"
 import MyButton from "../../components/Button"
+import api from "../../services/api"
 import BackgroundImage from "../../components/ImageHeader"
 
 const ListUsers = () => {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const {data} = await api.get('/usuarios')
+            setUsers(data)
+        }
+        getUsers()
+    }, [])
+    
     return (
         <div>
             <BackgroundImage />
             <h1>Listagem de Usuários</h1>
+            {users.map((user) => (
+                <p key={user.id}>{user.name} - {user.age} - {user.email}</p>
+            ))}
             <MyButton>Voltar</MyButton>
         </div>
     )
